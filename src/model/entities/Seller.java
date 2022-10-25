2e89f6c6ecf4af06d6a34dd6b0c416a8922b65ce
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2022-10-25 07:22:56 -0300

    findbyId implements

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -4,12 +4,10 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-public class Seller implements Serializable{
-	
+public class Seller implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	private Integer id;
 	private String name;
 	private String email;
@@ -17,9 +15,11 @@ public class Seller implements Serializable{
 	private Double baseSalary;
 	private Department department;
 
-	
-	
-	//constructors
+	// constructors
+	public Seller() {
+		super();
+	}
+
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		super();
 		this.id = id;
@@ -30,85 +30,55 @@ public class Seller implements Serializable{
 		this.department = department;
 	}
 
-
-
-	public Seller() {
-		super();
-	}
-
-
-	// getters and setters 
+	// getters and setters
 
 	public Integer getId() {
 		return id;
 	}
 
-
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 
 	public Date getBirthDate() {
 		return birthDate;
 	}
 
-
-
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
-
-
 
 	public Double getBaseSalary() {
 		return baseSalary;
 	}
 
-
-
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
-
-
 
 	public Department getDepartment() {
 		return department;
 	}
 
-
-
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
-
 
 	// equals and hashcode
 
@@ -116,8 +86,6 @@ public class Seller implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -138,18 +106,5 @@ public class Seller implements Serializable{
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
