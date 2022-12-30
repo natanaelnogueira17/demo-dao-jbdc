c9097b079153baa82420eb9369eacc015f1f5835
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2022-12-30 16:21:54 -0300

    findbyId 2

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -17,7 +19,7 @@ public class SellerDaoJDBC implements SellerDao {
 	private Connection conn;
 	
 	public SellerDaoJDBC(Connection conn) {
-		this.conn = conn;
+		this.conn = conn; 
 	}
 
 	@Override
@@ -53,16 +55,9 @@ public class SellerDaoJDBC implements SellerDao {
 				st.setInt(1,  id);
 				rs = st.executeQuery();
 				if (rs.next()) {
-					Department dep = new Department();
-					dep.setId(rs.getInt("DepartmentId"));
-					dep.setName(rs.getString("DepName"));
-					Seller obj = new Seller();
-					obj.setId(rs.getInt("Id"));
-					obj.setName(rs.getString("Name"));
-					obj.setEmail(rs.getString("Email"));
-					obj.setBaseSalary(rs.getDouble("BaseSalary"));
-					obj.setBirthDate(rs.getDate("BirthDate"));
-					obj.setDepartment(dep);
+					Department dep = instantiateDepartment(rs);					
+					Seller obj = instantiateSeller(rs, dep);						
+					
 					return obj;
 					
 				
@@ -72,18 +67,94 @@ public class SellerDaoJDBC implements SellerDao {
 			catch(SQLException e) {
 			throw new DbException(e.getMessage());
 		}
-			finnaly{
-				DB.closeStatement(st);
-				DB.closeResultSet(rs);
-			}
+			
 		}
 		
 	
 
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+	Seller obj	= new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+				+"WHERE DepartmentId = ?"
+					+"order BY name ");	
+			//st.setInt(1,  department.getId());
+			rs = st.executeQuery();
+			List<Seller> list =  new ArrayList<>();
+			Map<Integer, Department>map =  new HashMap<>();
+			
+		while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep ==null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+				}
+			return list;
 	}
+		catch(SQLException e) {
+		throw new DbException(e.getMessage());
+	}
+		//return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+				
+					+"order BY name ");	
+			
+			rs = st.executeQuery();
+			List<Seller> list =  new ArrayList<>();
+			Map<Integer, Department>map =  new HashMap<>();
+			
+		while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep ==null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+				}
+			return list;
+	}
+		catch(SQLException e) {
+		throw new DbException(e.getMessage());
+	}
+		
+	}
+	
 
 }
