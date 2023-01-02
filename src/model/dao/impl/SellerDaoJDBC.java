3132e58166cce1e1b81168863fed5a6999ac7a73
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2023-01-02 14:18:58 -0300

    update implementations

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -26,13 +26,44 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+	public void update(Seller obj) {
+		PreparedStatement st = null;
+		try {
+			st =  conn.prepareStatement("update seller set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, Department = ? where id = ?")
+					
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			st.executeUpdate();
+			
+			int rowsAffected = st.executeUpdate();
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()){
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+				
+			}
+			
+		} catch (SQLException e ) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
 	@Override
-	public void update(Seller obj) {
+	public void insert(Seller obj) {
 		PreparedStatement st = null;
 		try {
 			st =  conn.prepareStatement("insert into seller(Name, Email, BirthDate, BaseSalary, Department) values (?, ?, ?, ?, ?)"
